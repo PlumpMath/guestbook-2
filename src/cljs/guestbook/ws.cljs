692cb7617f7dc1efac19fc0fc98b99c6fe32ce6b
Sava4 <sasha.savchenko@gmail.com>
2016-02-24 19:17:24 +0200

    Sente on the server
    Before sente on the client
    No testing done.

--- src/cljs/guestbook/ws.cljs
+++ src/cljs/guestbook/ws.cljs
@@ -1,24 +1,6 @@
 (ns guestbook.ws
-  (:require [cognitect.transit :as t]))
+  (:require [taoensso.sente :as sente]))
 
-(defonce ws-chan (atom nil))
-
-(def json-reader (t/reader :json))
-(def json-writer (t/writer :json))
-
-
-(defn receive-message! [handler]
-  (fn [msg]
-    (->> msg .-data (t/read json-reader) handler)))
-
-(defn send-message! [msg]
-  (if @ws-chan
-    (->> msg (t/write json-writer) (.send @ws-chan))
-    (throw (js/Error. "Websocket is not available!"))))
-
-(defn connect! [url receive-handler]
-  (if-let [chan (js/WebSocket. url)]
-    (do
-      (set! (.-onmessage chan) (receive-message! receive-handler))
-      (reset! ws-chan chan))
-    (throw (js/Error. "Websocket connection failed!"))))
+(let [connection (sente/make-channel-socket! "/ws" {:type :auto})]
+  (def ch-chsk (:ch-recv connection)) ;ChannelSocket's receive channel
+  (def send-message! (:send-fn connection)))
